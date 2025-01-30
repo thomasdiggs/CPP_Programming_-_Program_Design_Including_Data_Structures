11fdd9f18760bbdfb86ce2007bd748e9c62f7e94
Thomas Diggs <thomasdiggs@gmail.com>
2025-01-30 16:24:09 -0700

    Added boilerplate and instructions for Chapter 4 Programming Exercise 1

---
+++ Programming_Exercises/Chapter_04/1/my_program.cpp
@@ -0,0 +1,20 @@
+
+/* C++ Programming: Program Design Including Data Structures
+ * Chapter 4, Programming Exercise 1
+ *
+ *
+ * Write a program that prompts the user to input a number. The pro-
+ * gram should then output the number and a message saying whether
+ * the number is positive, negative, or zero.
+ */
+
+
+#include <iostream>
+
+int main()
+{
+
+
+    return 0;
+}
+
