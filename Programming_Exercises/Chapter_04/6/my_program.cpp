4cba37a0838e236c682acb3b596f4e57a1b231f7
Thomas Diggs <thomasdiggs@gmail.com>
2025-02-06 19:00:05 -0700

    Chapter 4 Exercises complete

---
+++ Programming_Exercises/Chapter_04/6/my_program.cpp
@@ -0,0 +1,27 @@
+/* C++ Programming: Program Design Including Data Structures
+ * Chapter 4, Programming Exercise 6
+ *
+ *
+ * A box of cookies can hold 24 cookies, and a container can hold 75
+ * boxes of cookies. Write a program that prompts the user to enter
+ * the total number of cookies, the number of cookies in a box, and the
+ * number of cookie boxes in a container. The program then outputs the
+ * number of boxes and the number of containers to ship the cookies.
+ * Note that each box must contain the specified number of cookies, and
+ * each container must contain the specified number of boxes. If the last
+ * box of cookies contains less than the number of specified cookies, you
+ * can discard it and output the number of leftover cookies. Similarly, if
+ * the last container contains less than the number of specified boxes,
+ * you can discard it and output the number of leftover boxes.
+ */
+
+
+#include <iostream>
+
+int main()
+{
+	
+
+	return 0;
+}
+
